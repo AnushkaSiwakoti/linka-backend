cf8e7422376dcad1c89842bac9d3ff6c424f43bf
Anushka <asiwakot@stedwards.edu>
2024-10-03 22:49:41 -0500

    idek

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -69,7 +69,7 @@ DATABASES = {
         'PASSWORD': 'Linka2024!',
         # for local only
         # 'HOST': 'localhost', 
-        'HOST': 'localhost',  
+        'HOST': 'mysql',  
         'PORT': '3306',      
     }
 }
