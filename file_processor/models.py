13aa8431110e42369b4fffd895f5b40e7f585755
Anushka <asiwakot@stedwards.edu>
2024-10-17 21:30:15 -0500

    google drive api

--- file_processor/models.py
+++ file_processor/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 class ProcessedFileData(models.Model):
-    field1 = models.CharField(max_length=100)  #to store string data from the CSV
-    field2 = models.IntegerField()  #to store integer data from the CSV
-    uploaded_at = models.DateTimeField(auto_now_add=True)  # this automatically adds the timestamp of upload
+    file_id = models.CharField(max_length=100)  # To store the Google Drive file ID
+    file_name = models.CharField(max_length=255)  # Original file name
+    timestamp = models.DateTimeField(auto_now_add=True)  # Timestamp when the file was uploaded
+    file_url = models.URLField()  # To store the Google Drive file URL
+    processed = models.BooleanField(default=False)  # To indicate whether the file is processed or not
 
     def __str__(self):
-        return f'{self.field1} - {self.field2}'
+        return f'{self.file_name} - {self.file_id}'
