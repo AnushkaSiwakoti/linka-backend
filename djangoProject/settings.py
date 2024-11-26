a1ca71c5ccbd86a78abfe1db0ea8dbecf147d178
AnushkaSiwakoti <134111960+AnushkaSiwakoti@users.noreply.github.com>
2024-11-25 19:23:37 -0600

    Update settings.py

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -103,7 +103,11 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/deployments/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'deployments'),
+]
+
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
